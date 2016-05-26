f70c6ded1c2d8654f0e5e78bda7e11e54627339e
Monica Grandy <monica.grandy@gmail.com>
2016-05-26 14:40:12 -0700

    [feature] connects cityView and tableView

--- client/app/cityView/cityView.js
+++ client/app/cityView/cityView.js
@@ -1,10 +1,18 @@
 App.controller('cityCtrl', function($scope, Data) {
-  //gets data passed from tableView and renders it
-  // $scope.city = {
-  //   name: ''
-  //   forecast: ''
-  // };
+  // / Check if object doesn't exist, use session storage.
+  // This way, on refresh or back, it won't have all undefined values
+  if (!Data.clickedItem) {
+    Data.clickedItem = JSON.parse(sessionStorage.tempStorage);
+  }
 
+  if (Data.clickedItem) {
+    console.log("this is the clicked item: ", Data.clickedItem)
+    $scope.city = Data.clickedItem;
+    Data.getFiveDay($scope.city, function(forecastData){
+      console.log("forecast data: ", forecastData)
+      $scope.forecast = forecastData
+    })
+  }
 
 })
 
---
+++ client/app/factories.js
@@ -0,0 +1,54 @@
+App 
+.factory('Data', function($http){
+  var getAllData = function(cb){
+    console.log("inside getAllData")
+    $http({
+      method: 'POST',
+      url: '/api/fetchData'
+    }).then(function success(data){
+      var collection = data.data.map(function(city){
+        return {
+          city: city
+        };
+      });
+      cb(collection);
+    },
+  function error(err) {
+    console.log("ERROR: ", err)
+  })
+};
+
+ var addNew = function(city, cb){
+  console.log("inside addNew: ", city)
+  $http({
+    method: 'POST',
+    url: '/api/addData',
+    data: city
+  }).then(function success(data){
+    cb(data)
+  });
+ };
+
+var getFiveDay = function(cityName, cb){
+  console.log("inside fiveDay: ", cityName)
+  $http({
+    method: 'POST',
+    url: '/api/getForecast',
+    data: cityName
+  }).then(function success(data){
+    return {
+      data: data
+    };
+  });
+  cb(data)
+}; 
+
+var clickedItem = {} 
+
+return {
+    getAllData: getAllData,
+    addNew: addNew,
+    getFiveDay: getFiveDay,
+    clickedItem: clickedItem
+  }
+})
