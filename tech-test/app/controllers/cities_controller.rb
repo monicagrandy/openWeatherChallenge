dd3dfec471a0e0c4e152fd47920ded956f36a528
Monica Grandy <monica.grandy@gmail.com>
2016-05-27 07:52:29 -0700

    adds controller logic to check if record has been created

--- tech-test/app/controllers/cities_controller.rb
+++ tech-test/app/controllers/cities_controller.rb
@@ -1,7 +1,5 @@
 class CitiesController < ApplicationController
   require 'open_weather'
-  # protect_from_forgery with: :null_session
-  # respond_to :json
    def index
         # very simple code to grab all posts so they can be
         # displayed in the Index view (index.html.erb)
@@ -21,28 +19,31 @@ class CitiesController < ApplicationController
         response = OpenWeather::Forecast.city(params['_json'], options)
         city = response['city']
         list = response['list']
-        puts list[0]['weather'][0]['description']
-        newCity = City.new(
-          :name => city['name'], 
-          :lat => city['coord']['lat'], 
-          :long => city['coord']['lon'],
-          :currTemp => list[0]['main']['temp'], 
-          :currDay => list[0]['dt'], 
-          :currDesc => list[0]['weather'][0]['description'],
-          :day2 =>  list[1]['dt'],
-          :temp2 => list[1]['main']['temp'], 
-          :desc2 => list[1]['weather'][0]['description'],
-          :day3 =>  list[2]['dt'],
-          :temp3 => list[2]['main']['temp'], 
-          :desc3 => list[2]['weather'][0]['description'],
-          :day4 =>  list[3]['dt'],
-          :temp4 => list[3]['main']['temp'], 
-          :desc4 => list[3]['weather'][0]['description'],
-          :day5 =>  list[4]['dt'],
-          :temp5 => list[4]['main']['temp'], 
-          :desc5 => list[4]['weather'][0]['description'],
-          )
-        newCity.save
+        if City.where(:name => city['name']).blank?
+          newCity = City.new(
+            :name => city['name'], 
+            :lat => city['coord']['lat'], 
+            :long => city['coord']['lon'],
+            :currTemp => list[0]['main']['temp'], 
+            :currDay => list[0]['dt'], 
+            :currDesc => list[0]['weather'][0]['description'],
+            :day2 =>  list[1]['dt'],
+            :temp2 => list[1]['main']['temp'], 
+            :desc2 => list[1]['weather'][0]['description'],
+            :day3 =>  list[2]['dt'],
+            :temp3 => list[2]['main']['temp'], 
+            :desc3 => list[2]['weather'][0]['description'],
+            :day4 =>  list[3]['dt'],
+            :temp4 => list[3]['main']['temp'], 
+            :desc4 => list[3]['weather'][0]['description'],
+            :day5 =>  list[4]['dt'],
+            :temp5 => list[4]['main']['temp'], 
+            :desc5 => list[4]['weather'][0]['description'],
+            )
+          newCity.save
+        else
+          puts "city has already been added" 
+        end   
       end
 
       private
